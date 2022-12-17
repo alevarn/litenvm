f753bee0683f0cb2ed872558c849785da20f42af
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-12-17 14:06:21 +0100

    add cmocka with two sample test files

---
+++ core/test/sample2_test.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <cmocka.h>
+
+#include "sample.h"
+
+void get_magic_number_test(void **state)
+{
+    assert_int_equal(get_magic_number(), MY_MAGIC_NUMBER);
+}
+
+int main()
+{
+    const struct CMUnitTest tests[] =
+        {
+            cmocka_unit_test(get_magic_number_test)
+        };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
