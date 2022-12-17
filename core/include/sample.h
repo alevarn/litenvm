439ef727eea9cb9e9b8564319bb25a2d9b2fcb71
Marcus Alevärn <42449446+alevarn@users.noreply.github.com>
2022-12-17 14:15:39 +0100

    Issue-1: Add the cmocka framework (#2)

---
+++ core/include/sample.h
@@ -0,0 +1,12 @@
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+#define MY_MAGIC_NUMBER 5
+
+int get_magic_number();
+
+int get_two();
+
+const char *get_hello_str();
+
+#endif
---
+++ core/src/sample.c
@@ -0,0 +1,16 @@
+#include "sample.h"
+
+int get_magic_number()
+{
+    return MY_MAGIC_NUMBER;
+}
+
+int get_two()
+{
+    return 2;
+}
+
+const char *get_hello_str()
+{
+    return "Hello";
+}
