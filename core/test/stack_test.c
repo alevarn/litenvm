b9cc870954538b350b4023717ae4693760a26154
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-12-21 13:53:34 +0100

    update test file

--- core/test/stack_test.c
+++ core/test/stack_test.c
@@ -3,10 +3,12 @@
 #include "config.h"
 #include "stack.h"
 
+#define STACK_INITIAL_CAPACITY 8
+
 void stack_new_test(void **state)
 {
     Stack stack = stack_new(sizeof(int));
-    assert_int_equal(8, stack.capacity);
+    assert_int_equal(STACK_INITIAL_CAPACITY, stack.capacity);
     assert_int_equal(0, stack.length);
     assert_int_equal(sizeof(int), stack.elemsize);
     stack_free(&stack);
@@ -17,12 +19,12 @@ void stack_single_push_pop_test(void **state)
     Stack stack = stack_new(sizeof(int));
     int item = 100;
     stack_push(&stack, &item);
-    assert_int_equal(8, stack.capacity);
+    assert_int_equal(STACK_INITIAL_CAPACITY, stack.capacity);
     assert_int_equal(1, stack.length);
     assert_int_equal(sizeof(int), stack.elemsize);
     assert_int_equal(100, *(int *)stack_top(&stack));
     stack_pop(&stack);
-    assert_int_equal(8, stack.capacity);
+    assert_int_equal(STACK_INITIAL_CAPACITY, stack.capacity);
     assert_int_equal(0, stack.length);
     assert_int_equal(sizeof(int), stack.elemsize);
     stack_free(&stack);
@@ -47,14 +49,14 @@ void stack_multiple_push_pop_test(void **state)
         assert_int_equal(i, stack.length);
     }
 
-    assert_int_equal(8, stack.capacity);
+    assert_int_equal(STACK_INITIAL_CAPACITY, stack.capacity);
 
     stack_free(&stack);
 }
 
 int main()
 {
-    set_config(test_malloc_func, test_realloc_func, test_free_func, 8);
+    set_config(test_malloc_func, test_realloc_func, test_free_func, STACK_INITIAL_CAPACITY);
 
     const struct CMUnitTest tests[] =
         {
